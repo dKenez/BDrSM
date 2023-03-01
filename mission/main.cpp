06de51869c9e2d437e0d1c45def52eb99badefe9
dKenez <47757462+dKenez@users.noreply.github.com>
2023-03-01 11:42:40 +0100

    pls work uwu

--- mission/main.cpp
+++ mission/main.cpp
@@ -158,6 +158,7 @@ void step4()
 
     // start this mission
     bridge.tx("regbot:# liwi\n");
+    bridge.tx("regbot liwi\n");
     // wait until finished
     //
     //   cout << "Waiting for step 1 to finish (event 0 is send, when mission is finished)\n";
